c324a914107cfeea1996917e034c9033daefdf00
Mikita Yankousk <nikitayankouski@gmail.com>
2023-02-11 22:54:32 +0100

    init re-commit

---
+++ wiki-reader/src/test/java/com/hudman/wikireader/WikiReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudman.wikireader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
