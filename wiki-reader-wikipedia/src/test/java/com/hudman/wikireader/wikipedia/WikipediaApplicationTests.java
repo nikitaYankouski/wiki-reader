6e339d2853aab11019e2d8c87c510568ae611482
Mikita Yankousk <nikitayankouski@gmail.com>
2023-03-01 14:09:13 +0300

    re-arch project - init state last

---
+++ wiki-reader-wikipedia/src/test/java/com/hudman/wikireader/wikipedia/WikipediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudman.wikireader.wikipedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikipediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
