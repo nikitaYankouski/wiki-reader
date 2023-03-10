888d2bb21b0186cec74d76d1250bc8e8f3752c20
Mikita Yankousk <nikitayankouski@gmail.com>
2023-03-10 11:06:27 +0300

    review builders

--- wiki-reader-wikipedia/src/main/java/com/hudman/wikireader/wikipedia/builder/QueryExtractBuilder.java
+++ wiki-reader-wikipedia/src/main/java/com/hudman/wikireader/wikipedia/builder/QueryExtractBuilder.java
@@ -2,9 +2,10 @@ package com.hudman.wikireader.wikipedia.builder;
 
 import org.apache.hc.core5.net.URIBuilder;
 
-import java.util.List;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-class QueryExtractBuilder {
+public class QueryExtractBuilder {
     private final static String ACTION = "query";
     private final static String FORMAT = "json";
     private final static String PROP = "extracts";
@@ -12,27 +13,54 @@ class QueryExtractBuilder {
     private final static String EXPLAIN_TEXT = "1";
     private final static String EXSECTION_FORMAT = "wiki";
 
-    private String language;
-    private List<Long> padeIds;
+    public static URI build(Language language, long[] pageId) throws URISyntaxException {
+        var host = language.getValue() + "." + "wikipedia.org";
+        var connectedPageId = concatPadeId(pageId);
 
-    public QueryExtractBuilder(Language language, List<Long> padeIds) {
-        this.language = language.getLanguage();
-        this.padeIds = padeIds;
-    }
-
-    public String build() {
-        var uriBuilder = new URIBuilder()
+        return new URIBuilder()
                 .setScheme("https")
-                .setHost(language.concat(".").concat("wikipedia.org"))
+                .setHost(host)
                 .setPath("/w/api.php")
                 .setParameter("action", ACTION)
                 .setParameter("format", FORMAT)
                 .setParameter("prop", PROP)
-                .setParameter("padeids", String.valueOf(padeIds.get(0)))
+                .setParameter("pageids", connectedPageId)
                 .setParameter("formatversion", FORMAT_VERSION)
                 .setParameter("explaintext", EXPLAIN_TEXT)
-                .setParameter("exsectionformat", EXSECTION_FORMAT);
+                .setParameter("exsectionformat", EXSECTION_FORMAT)
+                .build();
+    }
 
-        return uriBuilder.toString();
+    public static URI buildContinue(URI uri, long[] pageId, int number) throws URISyntaxException {
+        var scheme = uri.getScheme();
+        var host = uri.getHost();
+        var path = uri.getPath();
+        var connectedPageId = concatPadeId(pageId);
+
+        return new URIBuilder()
+                .setScheme(scheme)
+                .setHost(host)
+                .setPath(path)
+                .setParameter("action", ACTION)
+                .setParameter("format", FORMAT)
+                .setParameter("prop", PROP)
+                .setParameter("continue", "||")
+                .setParameter("pageids", connectedPageId)
+                .setParameter("formatversion", FORMAT_VERSION)
+                .setParameter("explaintext", EXPLAIN_TEXT)
+                .setParameter("exsectionformat", EXSECTION_FORMAT)
+                .setParameter("excontinue", String.valueOf(number))
+                .build();
+    }
+
+    private static String concatPadeId(long[] pageId) {
+        var response = new StringBuilder();
+        for (int count = 0; count < pageId.length; count++) {
+            response.append(pageId[count]);
+            if (count + 1 != pageId.length) {
+                response.append("|");
+            }
+        }
+        return response.toString();
     }
 }
--- wiki-reader-wikipedia/src/main/java/com/hudman/wikireader/wikipedia/builder/QueryRandomBuilder.java
+++ wiki-reader-wikipedia/src/main/java/com/hudman/wikireader/wikipedia/builder/QueryRandomBuilder.java
@@ -2,35 +2,28 @@ package com.hudman.wikireader.wikipedia.builder;
 
 import org.apache.hc.core5.net.URIBuilder;
 
-public class QueryRandomBuilder implements BaseBuilder {
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class QueryRandomBuilder {
     private final static String ACTION = "query";
     private final static String FORMAT = "json";
     private final static String LIST = "random";
     private final static String FORMAT_VERSION = "latest";
-    private String language;
-    private String rnNameSpace;
-    private String rnLimit;
 
-    public QueryRandomBuilder(Language language, String rnNameSpace, String rnLimit) {
-        this.language = language.getLanguage();
-        this.rnNameSpace = rnNameSpace;
-        this.rnLimit = rnLimit;
-    }
-
-    @Override
-    public String build() {
-        var uriBuilder = new URIBuilder()
+    public static URI build(Language language, String rnNameSpace, String rnLimit) throws URISyntaxException {
+        var host = language.getValue() + "." + "wikipedia.org";
+        return new URIBuilder()
                 .setScheme("https")
-                .setHost(language.concat(".").concat("wikipedia.org"))
+                .setHost(host)
                 .setPath("/w/api.php")
                 .setParameter("action", ACTION)
                 .setParameter("format", FORMAT)
                 .setParameter("list", LIST)
                 .setParameter("formatversion", FORMAT_VERSION)
                 .setParameter("rnnamespace", rnNameSpace)
-                .setParameter("rnlimit", rnLimit);
-
-        return uriBuilder.toString();
+                .setParameter("rnlimit", rnLimit)
+                .build();
     }
 
 }
