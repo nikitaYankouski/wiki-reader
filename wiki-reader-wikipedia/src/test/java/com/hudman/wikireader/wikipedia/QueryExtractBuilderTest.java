b49f9f68a84d6df744fa1e68ad977f8246ab94b3
Mikita Yankousk <nikitayankouski@gmail.com>
2023-03-10 11:05:53 +0300

    add tests for QueryExtractBuilder

---
+++ wiki-reader-wikipedia/src/test/java/com/hudman/wikireader/wikipedia/QueryExtractBuilderTest.java
@@ -0,0 +1,33 @@
+package com.hudman.wikireader.wikipedia;
+
+import com.hudman.wikireader.wikipedia.builder.Language;
+import com.hudman.wikireader.wikipedia.builder.QueryExtractBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QueryExtractBuilderTest {
+
+    long[] pageId = new long[] { 58143961, 62576755, 3752653, 36554043, 51135969 };
+
+    @Test
+    void checkBuildURI() throws URISyntaxException {
+        var uri = QueryExtractBuilder.build(Language.ENGLISH, pageId);
+
+        var expected = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&pageids=58143961%7C62576755%7C3752653%7C36554043%7C51135969&formatversion=latest&explaintext=1&exsectionformat=wiki";
+
+        assertEquals(expected, uri.toString());
+    }
+
+    @Test
+    void checkBuildContinueURI() throws URISyntaxException {
+        var uri = QueryExtractBuilder.build(Language.ENGLISH, pageId);
+        var uriContinue = QueryExtractBuilder.buildContinue(uri, pageId, 1);
+
+        var expectedContinue = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&continue=%7C%7C&pageids=58143961%7C62576755%7C3752653%7C36554043%7C51135969&formatversion=latest&explaintext=1&exsectionformat=wiki&excontinue=1";
+
+        assertEquals(expectedContinue, uriContinue.toString());
+    }
+}
--- wiki-reader-wikipedia/src/test/java/com/hudman/wikireader/wikipedia/QueryRandomBuilderTest.java
+++ wiki-reader-wikipedia/src/test/java/com/hudman/wikireader/wikipedia/QueryRandomBuilderTest.java
@@ -4,20 +4,18 @@ import com.hudman.wikireader.wikipedia.builder.Language;
 import com.hudman.wikireader.wikipedia.builder.QueryRandomBuilder;
 import org.junit.jupiter.api.Test;
 
+import java.net.URISyntaxException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class QueryRandomBuilderTest {
 
     @Test
-    void checkBuildURI() {
-        final var builder = new QueryRandomBuilder(Language.ENGLISH, "0", "10");
-
-        final var uri = builder.build();
-
+    void checkBuildURI() throws URISyntaxException {
+        final var uri = QueryRandomBuilder.build(Language.ENGLISH, "0", "10");
         final var expected = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&formatversion=latest&rnnamespace=0&rnlimit=10";
-
-        assertEquals(expected, uri);
+        assertEquals(expected, uri.toString());
     }
 
 }
